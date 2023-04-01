23326c99ba286e25b4638e78c819ac4263691e71
Ryan Williams <3375653+ryankwilliams@users.noreply.github.com>
2023-04-01 06:07:26 -0400

    Enable pre-commit

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
-.DEFAULT_GOAL := lint
+.DEFAULT_GOAL := test
 
 .PHONY: lint
 lint:
 	ansible-lint meta/main.yml
 	ansible-lint tasks/main.yml
 	ansible-lint tests/test.yml
+
+.PHONY: test
+test:
+	pre-commit run --all-files --show-diff-on-failure
--- library/ssh_copy_id.py
+++ library/ssh_copy_id.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python
 import socket
-from os.path import join, isfile
+from os.path import isfile
+from os.path import join
 
 import paramiko
 from ansible.module_utils.basic import AnsibleModule
 
 ANSIBLE_METADATA = {
-    'metadata_version': '1.0',
-    'status': ['stable'],
-    'supported_by': 'community'
+    "metadata_version": "1.0",
+    "status": ["stable"],
+    "supported_by": "community",
 }
 
-DOCUMENTATION = '''
+DOCUMENTATION = """
 ---
 module: ssh_copy_id
 
@@ -49,9 +50,9 @@ options:
         required: false
 author:
     - Ryan Williams
-'''
+"""
 
-EXAMPLES = '''
+EXAMPLES = """
 # Inject SSH public key into remote system
 - name: Inject SSH public key
   ssh_copy_id:
@@ -61,9 +62,9 @@ EXAMPLES = '''
     ssh_public_key: /home/user/.ssh/id_rsa.pub
     hetzner_storagebox: false
     ssh_port: 22
-'''
+"""
 
-RETURN = '''
+RETURN = """
 changed:
     description: Whether the module performed an action or not
     type: bool
@@ -73,62 +74,55 @@ message:
 original_message:
     description: The original name param that was passed in
     type: str
-'''
+"""
 
 
 def run_module():
     # module arguments
     module_args = dict(
-        hostname=dict(type='str', required=True),
-        username=dict(type='str', required=True),
-        password=dict(type='str', required=True, no_log=True),
-        ssh_public_key=dict(type='str', required=True),
-        hetzner_storagebox=dict(type='str', required=False),
-        ssh_port=dict(type='str', required=False)
+        hostname=dict(type="str", required=True),
+        username=dict(type="str", required=True),
+        password=dict(type="str", required=True, no_log=True),
+        ssh_public_key=dict(type="str", required=True),
+        hetzner_storagebox=dict(type="str", required=False),
+        ssh_port=dict(type="str", required=False),
     )
 
     # results dictionary
-    result = dict(
-        changed=False,
-        original_message='',
-        message=''
-    )
+    result = dict(changed=False, original_message="", message="")
 
     # create ansible module object
-    module = AnsibleModule(
-        argument_spec=module_args,
-        supports_check_mode=True
-    )
+    module = AnsibleModule(argument_spec=module_args, supports_check_mode=True)
 
     # set local variables
-    hostname = module.params['hostname']
-    username = module.params['username']
-    password = module.params['password']
-    public_key = module.params['ssh_public_key']
-    hetzner_storagebox = module.params['hetzner_storagebox']
-    port = module.params['ssh_port']
+    hostname = module.params["hostname"]
+    username = module.params["username"]
+    password = module.params["password"]
+    public_key = module.params["ssh_public_key"]
+    hetzner_storagebox = module.params["hetzner_storagebox"]
+    port = module.params["ssh_port"]
 
     # MODULE TASKS TO BE PERFORMED BELOW..
 
     # set authorized key path
     # Add Hetzner Storage-box support
-    if hetzner_storagebox.lower() == 'true':
-        auth_key = '.ssh/authorized_keys'
+    if hetzner_storagebox.lower() == "true":
+        auth_key = ".ssh/authorized_keys"
     else:
-        if username == 'root':
-            base_dir = '/root/'
-            auth_key = join(base_dir, '.ssh/authorized_keys')
+        if username == "root":
+            base_dir = "/root/"
+            auth_key = join(base_dir, ".ssh/authorized_keys")
         else:
-            base_dir = '/home/%s' % username
-            auth_key = join(base_dir, '.ssh/authorized_keys')
+            base_dir = "/home/%s" % username
+            auth_key = join(base_dir, ".ssh/authorized_keys")
 
     # prior to creating ssh connection via paramiko, lets verify the public
     # key supplied exists on disk
     if isfile(public_key):
-        module.log('SSH public key %s exists!' % public_key)
+        module.log("SSH public key %s exists!" % public_key)
     else:
-        result['message'] = 'Unable to locate ssh public key %s' % public_key
-        module.fail_json(msg=result['message'])
+        result["message"] = "Unable to locate ssh public key %s" % public_key
+        module.fail_json(msg=result["message"])
         module.exit_json(**result)
 
     # create ssh client via paramiko
@@ -143,67 +137,65 @@ def run_module():
             password=password,
             look_for_keys=False,
             allow_agent=False,
-            port=port
+            port=port,
         )
-    except (paramiko.BadHostKeyException, paramiko.AuthenticationException,
-        paramiko.SSHException, socket.error) as ex:
-        result['message'] = ex
-        module.fail_json(msg='Connection failed to %s' % hostname, **result)
+    except (
+        paramiko.BadHostKeyException,
+        paramiko.AuthenticationException,
+        paramiko.SSHException,
+        socket.error,
+    ) as ex:
+        result["message"] = ex
+        module.fail_json(msg="Connection failed to %s" % hostname, **result)
         module.exit_json(**result)
 
     # create sftp client
     sftp_con = ssh_con.open_sftp()
 
     # read ssh public key
-    with open(public_key, 'r') as fh:
+    with open(public_key, "r") as fh:
         data = fh.read()
 
     add_key = True
 
     # read remote system authorized key (if applicable)
     try:
-        file_reader = sftp_con.open(
-            auth_key,
-            mode='r'
-        )
+        file_reader = sftp_con.open(auth_key, mode="r")
         authorized_key_data = file_reader.read()
         file_reader.close()
 
         # check if key already exists
-        if data in authorized_key_data.decode('utf-8'):
-            result['message'] = 'SSH public key already injected!'
-            result['changed'] = False
-            module.log(result['message'])
+        if data in authorized_key_data.decode("utf-8"):
+            result["message"] = "SSH public key already injected!"
+            result["changed"] = False
+            module.log(result["message"])
             add_key = False
     except IOError:
-        module.warn('Authorized keys file %s not found!' % auth_key)
+        module.warn("Authorized keys file %s not found!" % auth_key)
 
         # make the .ssh directory
-        if hetzner_storagebox.lower() == 'true':
-            ssh_dir = '.ssh'
+        if hetzner_storagebox.lower() == "true":
+            ssh_dir = ".ssh"
         else:
-            ssh_dir = '/'.join(auth_key.split('/')[:-1])
+            ssh_dir = "/".join(auth_key.split("/")[:-1])
 
         try:
             sftp_con.lstat(ssh_dir)
         except IOError:
-            module.warn('%s user .ssh dir not found! Creating..' % username)
+            module.warn("%s user .ssh dir not found! Creating.." % username)
             sftp_con.mkdir(ssh_dir)
 
     # inject ssh public key into authorized keys
     if add_key:
-        file_handler = sftp_con.file(
-            auth_key,
-            mode='a'
-        )
+        file_handler = sftp_con.file(auth_key, mode="a")
         file_handler.write(data)
         file_handler.flush()
-        file_handler.chmod(int('0600', 8))
+        file_handler.chmod(int("0600", 8))
         file_handler.close()
 
-        result['message'] = 'SSH public key injected!'
-        result['changed'] = True
-        module.log(result['message'])
+        result["message"] = "SSH public key injected!"
+        result["changed"] = True
+        module.log(result["message"])
 
     # close sftp/ssh connection
     sftp_con.close()
@@ -217,5 +209,5 @@ def main():
     run_module()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
